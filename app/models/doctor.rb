292f9eba7dbf011329adadc859b52ea7f006df77
Ivan Dunaysky <dunaysky@gmail.com>
2023-05-20 05:32:13 -0700

    Refactored doctors model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -6,8 +6,8 @@ class Doctor < ApplicationRecord
   has_many   :users, through: :doctor_users
   has_one    :profile, as: :related_to, dependent: :destroy
 
-  scope :without_profile, -> { select { |doctor| doctor.profile.nil? } }
-  scope :with_profile, -> { select { |doctor| doctor.profile.present? } }
+  scope :without_profile, -> { where.not(id: Doctor.joins(:profile)) }
+  scope :with_profile, -> { where(id: Doctor.joins(:profile)) }
 
   def display_name
     "#{profile ? "#{profile.first_name} #{profile.last_name}" : "Id:#{id}"} | #{category.name}"
